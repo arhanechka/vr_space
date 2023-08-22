e84a7abc6d641fb9158f5156dad87330cbc3a816
arhanechka <arhanechka@gmail.com>
2023-08-22 00:03:47 -0400

    images added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './assets/hands.avif';
 import logo2 from './assets/logo4.png';
+import bg_img from './assets/bg_img1.avif';
+import bg_img1 from './assets/bg_img2.avif';
+import bg_img2 from './assets/logo1.avif';
+import women from './assets/women.avif';
+
 import Header from './components/header';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/home';
@@ -7,27 +12,48 @@ import About from './components/about'
 import Contacts from './components/contacts';
 import Booking from './components/Bookings';
 import VRGameList from './components/VRGameList';
+import { useNavigate, useLocation } from "react-router-dom";
 
 import './App.css';
 
 
 
 function App() {
+  const location = useLocation();
+  let currentLocation = location.pathname;
+  console.log(currentLocation)
+
+  const bgs = {
+    "/": logo,
+    "/about": women,
+    "/contacts": bg_img,
+    "/games": bg_img1,
+    "/booking": bg_img2
+  }
 
   const backgroundImage = {
-    backgroundImage: `url(${logo})`,
+    backgroundImage: `url(${bgs[currentLocation]})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     width: '100%',
-    height: '100vh',
+    height: '100%',
   }; 
+
+
+
+  let navigate = useNavigate();
+
+  const routeChange = () => {
+    let path = `/`;
+    navigate(path, {replace: true });
+  };
   return (
     <div className="App">
       <header className="App-header">
       <div style={backgroundImage}>
-        <img src={logo2} className="App-logo" alt="logo" />
-
+        <img src={logo2} className="App-logo" alt="logo" onClick={routeChange}/>
       <Header/>
+
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -38,8 +64,28 @@ function App() {
       </div>
      
       </header>
-     
+      
+      {/* <div className="App-header" >
+      <div style={backgroundImage}>
+        <Header/>
+        <Booking/>
+     </div>
+     </div>
+     <div className="App-header">
+      <div style={backgroundImage}>
+        <Header/>
+        <VRGameList/>
+     </div>
+     </div>
+
+     <div className="App-header">
+      <div style={backgroundImage}>
+        <Header/>
+        <Contacts/>
+     </div>
+     </div> */}
     </div>
+    
   );
 }
 
